b06c99104ea956718285b531835460896c9fdb3a
Wade Wegner <wade.wegner@gmail.com>
2018-02-13 06:13:22 -0800

    Updates to post-setup.sh

--- bin/post-setup.sh
+++ bin/post-setup.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
+TARGET_ORG_ALIAS=${1:-}
+STAGE=${2:-}
+
 ### Not required
 ### This script will execute after the org has been setup
 
 echo "hello from post-setup.sh"
+
+if [ "$STAGE" == "" ]; then
+
+  echo "only run from review app or ci"
+  
+fi
