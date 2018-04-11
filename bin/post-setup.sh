5a34928352ec44bb04990de6bbbf319ebf8b36c9
Dave Carroll <djcarroll@gmail.com>
2018-04-10 17:15:39 -0700

    test

--- bin/post-setup.sh
+++ bin/post-setup.sh
@@ -6,4 +6,4 @@ STAGE=${2:-}
 ### Not required
 ### This script will execute after the org has been setup
 sfdx force:user:permset:assign -n dreamhouse
-sfdx force:data:plan:import -p assets/data/Broker__c-Property__c-plan.json
+sfdx force:data:tree:import -p assets/data/Broker__c-Property__c-plan.json
