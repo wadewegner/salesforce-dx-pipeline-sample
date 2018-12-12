1ae49a313b4aba4da8dbb49e1152a87ecf19dd9f
hkolasani <hkolasani@hura.io>
2018-12-11 16:15:03 -0800

    Featuer 2

--- destroy1544560022.sh
+++
@@ -1,5 +0,0 @@
-heroku pipelines:destroy pipeline1544560022
-heroku apps:destroy -a dev1544560022 -c dev1544560022
-heroku apps:destroy -a staging1544560022 -c staging1544560022
-heroku apps:destroy -a prod1544560022 -c prod1544560022
-rm -- "destroy1544560022.sh"
